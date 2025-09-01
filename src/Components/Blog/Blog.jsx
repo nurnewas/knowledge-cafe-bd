1ed9ed7f61868ac1d0e083b6cb5bd97a407c939d
nurnewas <nurnewasjr@gmail.com>
2025-09-01 22:07:43 +0600

    Blog structure on img sextion and Tag section

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,14 +1,34 @@
 const Blog = ({ blogs }) => {
-  const { author, authorImg, title, postDate, readingTime, cover } = blogs;
+  const { author, authorImg, title, postDate, readingTime, cover, hashtags } =
+    blogs;
   return (
     <div>
       <img src={cover} alt="" />
+      <div className="flex justify-between">
+        <div>
+          <div className="flex gap-4 mt-2 ">
+            <img
+              src={authorImg}
+              alt={author}
+              className=" mt-1 w-14 rounded-tr-4xl  rounded-bl-3xl rounded-tl-2xl"
+            />
+            <div className="mt-2 ">
+              <h2 className="text-2xl">Author: {author}</h2>
+
+              <p>{postDate} </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <span>📅| ⏱ {readingTime}</span>
+        </div>
+      </div>
       <h1>Post title: {title}</h1>
-      <p>✍️ Author: {author}</p>
-      <img src={authorImg} alt={author} width="80" />
-      <p>
-        📅 {postDate} | ⏱ {readingTime}
-      </p>
+      {hashtags.map((hash, index) => (
+        <span key={index} className="text-blue-600 mr-2">
+          <a href=""># {hashtags}</a>
+        </span>
+      ))}
     </div>
   );
 };
